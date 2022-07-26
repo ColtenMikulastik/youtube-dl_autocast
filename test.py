e3da6672192be99e3ef1925801afa031bf0fbf3c
Colten <mikulastikc@gmail.com>
2022-07-25 20:13:46 -0500

    added some options that will extract audio

--- test.py
+++ test.py
@@ -3,20 +3,26 @@ import yt_dlp
 def yt_dlp_download(youtube_url):
     # options need to be specified
     yt_options = {
-            "outtmpl" : "%(playlist_index)s %(title)s.%(ext)s",
-            "audio_format" : "mp3",
-            "format" : "audio_only",
-            "path": "testdir",
+            "outtmpl": "testdir/%(playlist_index)s %(title)s.%(ext)s",
+            "format": "bestaudio/best",
+            "ignoreerrors": True,
+            "retries": 10,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192"
+                }
+                ]
         }
     # creating an instance of the YoutubeDL object
     # put options in the constructor's parameters
-    yt_downloader = yt_dlp.YoutubeDL(yt_options)
-    yt_downloader.download(youtube_url)
-    
+    with yt_dlp.YoutubeDL(yt_options) as ydl:
+        ydl.download(youtube_url)
 
 
 def main():
-    test_url = "https://www.youtube.com/watch?v=RTB5XhjbgZA&ab_channel=Posy"
+    test_url = "https://www.youtube.com/playlist?list=PLv0jjxv5Ng1FNkrmKzUIdxYNqiE4W3-J5"
     yt_dlp_download(test_url)
     print("done")
 
