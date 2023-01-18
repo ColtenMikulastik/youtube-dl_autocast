06ea7c570c86e29421b1d411bc81e4b675cb3669
Colten <mikulastikc@gmail.com>
2023-01-17 23:28:46 -0600

    fixed zero padding issue

--- mp3set.sh
+++ mp3set.sh
@@ -21,7 +21,7 @@ do
                         unpathed_album=$(echo "$album" | awk -F'/' '{print $NF}')
                         unpathed_song=$(echo "$song" | awk -F'/' '{print $NF}')
                         song_number=$(echo "$unpathed_song" | awk -F' ' '{print $1}')
-                        song_name=$(echo "$unpathed_song" | cut -c 3-)
+                        song_name=$(echo "$unpathed_song" | cut -d' ' -f2-)
                         echo "$song_name is song number ($song_number) on the album: $unpathed_album by $unpathed_artist in the genre $unpathed_genre"
                         # actually change the metadata
                         id3tool --set-title="$song_name" --set-album="$unpathed_album" --set-artist="$unpathed_artist" --set-genre-word="$unpathed_genre" --set-track="$song_number" "$song"
